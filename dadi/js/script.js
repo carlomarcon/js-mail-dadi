790df3f20a3b9b020623083c7736bc820656f239
carlomarcon <carlomarcon1998@gmail.com>
2023-10-23 18:15:30 +0200

    ADD: VERSION WITH EVENT LISTENER

--- dadi/js/script.js
+++ dadi/js/script.js
@@ -1,15 +1,20 @@
-const UserNumber = Math.floor(Math.random() * (6 - 1 + 1) + 1);
-const ComputerNumber = Math.floor(Math.random() * (6 - 1 + 1) + 1);
+// const UserNumber = Math.floor(Math.random() * (6 - 1 + 1) + 1);
+// const ComputerNumber = Math.floor(Math.random() * (6 - 1 + 1) + 1);
 const UserDice = document.querySelector(".container .dices:first-child");
 const ComputerDice = document.querySelector(".computer");
+const button = document.querySelector("button");
 
-UserDice.innerHTML = "<div class='result'>" + UserNumber + "</div>";
-ComputerDice.innerHTML = "<div class='result'>" + ComputerNumber + "</div>";
-
-if (UserNumber == ComputerNumber) {
-  document.querySelector("h1").innerHTML = "It's a draw !";
-} else if (UserNumber < ComputerNumber) {
-  document.querySelector("h1").innerHTML = "Computer wins !";
-} else {
-  document.querySelector("h1").innerHTML = "User wins !";
-}
+button.addEventListener("click", function () {
+  const UserNumber = Math.floor(Math.random() * (6 - 1 + 1) + 1);
+  const ComputerNumber = Math.floor(Math.random() * (6 - 1 + 1) + 1);
+  UserDice.innerHTML = "<div class='result'>" + UserNumber + "</div>";
+  ComputerDice.innerHTML = "<div class='result'>" + ComputerNumber + "</div>";
+  document.querySelector("h2").classList.add("info");
+  if (UserNumber == ComputerNumber) {
+    document.querySelector("h1").innerHTML = "It's a draw !";
+  } else if (UserNumber < ComputerNumber) {
+    document.querySelector("h1").innerHTML = "Computer wins !";
+  } else {
+    document.querySelector("h1").innerHTML = "User wins !";
+  }
+});
